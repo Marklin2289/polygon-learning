8340808c3beb511ce298136e4052d4036b9e1a14
zurgl <elayar.yacine@gmail.com>
2021-07-30 05:11:52 +0200

    patch network display

--- components/protocols/arweave/components/Nav.tsx
+++ components/protocols/arweave/components/Nav.tsx
@@ -6,11 +6,10 @@ const { Text, Paragraph } = Typography;
 
 export default function Nav() {
     const { state } = useAppState();
-    const { address, dataId, stateId } = state;
-    const networkId = 'arweave.testnet'
+    const { address, dataId, stateId, network } = state;
 
     const displaySecretKey = (address: string) => `${(address).slice(0,5)}...${(address).slice(-5)}`
-    const displayNetworkId = (networkId: string) => networkId
+    const displayNetwork = (network: string) => network
     const displayDataId = (dataId: string) => `${(dataId).slice(0,5)}...${(dataId).slice(-5)}`
     const displayStateId = (stateId: string) => `${(stateId).slice(0,5)}...${(stateId).slice(-5)}`
 
@@ -25,12 +24,12 @@ export default function Nav() {
 
     const AppState = () => {
         return (
-        <>
-            <Entry msg={"Network: "} value={networkId} display={displayNetworkId} />
-            {address && <Entry msg={"Address: "} value={address} display={displaySecretKey} />}
-            {dataId && <Entry msg={"dataTx: "} value={dataId} display={displayDataId} />}
-            {stateId && <Entry msg={"stateTx: "} value={stateId} display={displayStateId} />}
-        </>
+            <>
+                {network && <Entry msg={"Network: "} value={network} display={displayNetwork} />}
+                {address && <Entry msg={"Address: "} value={address} display={displaySecretKey} />}
+                {dataId && <Entry msg={"dataTx: "} value={dataId} display={displayDataId} />}
+                {stateId && <Entry msg={"stateTx: "} value={stateId} display={displayStateId} />}
+            </>
         )
     }
 
