0d52247bd9c6d8dda7fe7f4dee74d656287e2363
marklin2289 <marklin2289@gmail.com>
2022-12-14 22:14:10 -0800

    setValue()

--- components/protocols/polygon/challenges/setter.ts
+++ components/protocols/polygon/challenges/setter.ts
@@ -4,15 +4,18 @@ import {ethers} from 'ethers';
 declare let window: {
   ethereum: ethers.providers.ExternalProvider;
 };
-
 const setValue = async (contractAddress: string, value: number) => {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     // try to figure out the expected parameters
-    const contract = new ethers.Contract(undefined);
+    const contract = new ethers.Contract(
+      contractAddress,
+      SimpleStorageJson.abi,
+      signer,
+    );
     // try to figure out the expected method
-    const transactionResult = undefined;
+    const transactionResult = await contract.set(value, {gasLimit: 500000});
     const receipt = await transactionResult.wait();
     return {hash: receipt.transactionHash};
   } catch (error) {
