4de7c8d69799bc4ffbe740ffafe3233598b841ea
marklin2289 <marklin2289@gmail.com>
2022-12-14 19:45:49 -0800

    query.ts

--- components/protocols/polygon/challenges/query.ts
+++ components/protocols/polygon/challenges/query.ts
@@ -10,12 +10,25 @@ const query = async () => {
     const networkName = await provider.getNetwork().then((res) => {
       return res.name;
     });
+    // const networkProvider = await provider.getNetwork();
+    // const networkName = networkProvider.name;
 
     // TODO: Define the variables below
-    const chainId = undefined;
-    const blockHeight = undefined;
-    const gasPriceAsGwei = undefined;
-    const blockInfo = undefined;
+    // const chainId = await provider.getNetwork().then((res) => {
+    //   return res.chainId;
+    // });
+    const chainId = provider.network.chainId;
+
+    const blockHeight = await provider.getBlockNumber();
+    // const gasPriceAsGwei = ethers.utils.formatUnits(
+    //   await provider.getGasPrice(),
+    //   'gwei',
+    // );
+    const gasPriceAsGwei = await provider.getGasPrice().then((res) => {
+      return ethers.utils.formatUnits(res, 'gwei');
+    });
+
+    const blockInfo = await provider.getBlockWithTransactions(blockHeight);
 
     if (!chainId || !blockHeight || !gasPriceAsGwei || !blockInfo) {
       throw new Error('Please complete the code');
